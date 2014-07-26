ade334f80c7b0da5f22aac651716402982b8f6fd
agoldman <aprilgoldman@Aprils-MacBook-Air.local>
2014-07-26 11:41:19 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Vanstaurant::Application.config.secret_key_base = 'a478b87032dfa664195b3baf9ca02297cadaf707b6f9452f1109ddb2fb5ac013367b6cdc3a767688c4258b8c6c0c17765fd789554a326e9067f5139075fe34d9'
