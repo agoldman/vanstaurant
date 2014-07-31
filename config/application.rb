d573682becf6b8638ca83db6d766c51f95dcb9cf
agoldman <aprilgoldman@Aprils-MacBook-Air.local>
2014-07-31 13:59:04 -0700

    added custom font. league gothic! began welcome page layout

--- config/application.rb
+++ config/application.rb
@@ -20,6 +20,8 @@ module Vanstaurant
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
+    config.assets.paths << "#{Rails.root}/app/assets/fonts"
+
     config.assets.precompile += [
       'application.css.scss', 'custom.css.scss'
     ]
