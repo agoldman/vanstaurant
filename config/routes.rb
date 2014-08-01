ba3e0c4ec697e70af145e5270d15681748abc232
agoldman <aprilgoldman@Aprils-MacBook-Air.local>
2014-08-01 13:22:47 -0700

    adding devise and registration page

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,14 @@
 Vanstaurant::Application.routes.draw do
+  devise_for :users, :path => 'admin', :path_names => { :sign_in => "login", :sign_out => "logout", :sign_up => "register" }
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
   root 'pages#index'
-
   get 'about' => 'pages#about'
 
   resources :menus
+  resources :users
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
