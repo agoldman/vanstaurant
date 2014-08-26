8bd78b86629940d9f3bb742f4a7419f92f3338da
agoldman <aprilgoldman@Aprils-MacBook-Air.local>
2014-08-26 16:54:43 -0700

    styled login and admin pages with nice backgrounds and black center boxes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
-
   
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) << :name
@@ -16,7 +15,6 @@ class ApplicationController < ActionController::Base
   end
 
   def user_not_authorized
-    flash[:notice] = "Sign in as an admin. Vanstaurant is love."
     redirect_to new_user_session_path
   end
 
